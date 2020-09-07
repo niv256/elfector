d2815889dfcdba7d382048094f763cee931b5479
niv256 <niv25600@gmail.com>
2020-09-07 13:36:22 +0300

    Cleanup code

--- src/injector.cpp
+++ src/injector.cpp
@@ -1,40 +1,33 @@
-#include <iostream>
+#include "injector.hpp"
 #include <fcntl.h>
-#include <unistd.h>
+#include <iostream>
 #include <sys/stat.h>
 #include <sys/types.h>
-#include "injector.hpp"
+#include <unistd.h>
 
-Elf_target::Elf_target(std::string name) {
-	fd = open(name.c_str(), O_RDWR);
-	program_headers;
-	section_headers;
-}
+Elf_target::Elf_target(std::string name) { fd = open(name.c_str(), O_RDWR); }
 
-Elf_target::~Elf_target() {
-	close(fd);
-}
+Elf_target::~Elf_target() { close(fd); }
 
 void Elf_target::load_headers() {
-	Elf_target::load_main_header();
-	Elf_target::load_program_headers<Elf64_Phdr>(program_headers, header.e_phnum);
-	Elf_target::load_program_headers<Elf64_Shdr>(section_headers, header.e_shnum);
+  Elf_target::load_main_header();
+  Elf_target::load_program_headers(program_headers, header.e_phnum);
+  Elf_target::load_program_headers(section_headers, header.e_shnum);
 }
 
 void Elf_target::load_main_header() {
-	read(fd, static_cast<void*>(&header), sizeof(Elf64_Ehdr));
+  read(fd, static_cast<void *>(&header), sizeof(Elf64_Ehdr));
 }
 
-template<typename T>
-void Elf_target::load_program_headers(std::vector<T>& vector, int count) {
-	for (size_t i{}; i < count; i++) {
-		vector.push_back(get_header_type<T>());
-	}
+template <typename T>
+void Elf_target::load_program_headers(std::vector<T> &vector, int count) {
+  for (size_t i{}; i < count; i++) {
+    vector.push_back(get_header_type<T>());
+  }
 }
 
-template<typename T>
-T Elf_target::get_header_type() {
-	T object{};
-	read(fd, &object, sizeof(object));
-	return object;
+template <typename T> T Elf_target::get_header_type() {
+  T object{};
+  read(fd, &object, sizeof(object));
+  return object;
 }
--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,14 @@
 
-#include <iostream>
 #include "injector.hpp"
+#include <iostream>
 
 int main(int argc, char *argv[]) {
-	if (argc != 2) {
-		std::cout << "wrong arguments" << std::endl;
-		return 1;
-	}
+  if (argc != 2) {
+    std::cout << "wrong arguments" << std::endl;
+    return 1;
+  }
 
-	Elf_target target{ std::string{ argv[1] } };
-	target.load_headers();
-	std::cout << "executed" << std::endl;
+  Elf_target target{std::string{argv[1]}};
+  target.load_headers();
+  std::cout << "executed" << std::endl;
 }
